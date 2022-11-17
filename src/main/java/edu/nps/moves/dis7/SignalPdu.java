595cd9fe3d9e889fce969ffeb38aa16fb77d1bf1
Emily Marx <emilymarx@koverse.com>
2022-11-17 14:54:23 -0600

    Fixed marshaling issue with SignalPdu

--- src/main/java/edu/nps/moves/dis7/SignalPdu.java
+++ src/main/java/edu/nps/moves/dis7/SignalPdu.java
@@ -14,226 +14,239 @@ import java.io.*;
  */
 public class SignalPdu extends RadioCommunicationsFamilyPdu implements Serializable
 {
-   protected RadioCommsHeader header = new RadioCommsHeader();
-   /** encoding scheme used, and enumeration */
-   protected int  encodingScheme;
+  protected RadioCommsHeader header = new RadioCommsHeader();
+  /** encoding scheme used, and enumeration */
+  protected int  encodingScheme;
 
-   /** tdl type */
-   protected int  tdlType;
+  /** tdl type */
+  protected int  tdlType;
 
-   /** sample rate */
-   protected long  sampleRate;
+  /** sample rate */
+  protected long  sampleRate;
 
-   /** length od data */
-   protected short  dataLength;
+  /** length od data */
+  protected short  dataLength;
 
-   /** number of samples */
-   protected short  samples;
+  /** number of samples */
+  protected short  samples;
 
-   /** list of eight bit values */
-   protected byte[] data = new byte[0];
+  /** list of eight bit values */
+  protected byte[] data = new byte[0];
 
-/** Constructor */
- public SignalPdu()
- {
+  /** Constructor */
+  public SignalPdu()
+  {
     setPduType( (short)26 );
- }
+  }
 
-public int getMarshalledSize()
-{
-   int marshalSize = 0; 
+  public int getMarshalledSize()
+  {
+    int marshalSize = 0;
 
-   marshalSize = super.getMarshalledSize();
-   marshalSize = marshalSize + 2;  // encodingScheme
-   marshalSize = marshalSize + 2;  // tdlType
-   marshalSize = marshalSize + 4;  // sampleRate
-   marshalSize = marshalSize + 2;  // dataLength
-   marshalSize = marshalSize + 2;  // samples
-   marshalSize = marshalSize + data.length;
+    marshalSize = super.getMarshalledSize();
+    marshalSize = marshalSize + 2;  // encodingScheme
+    marshalSize = marshalSize + 2;  // tdlType
+    marshalSize = marshalSize + 4;  // sampleRate
+    marshalSize = marshalSize + 2;  // dataLength
+    marshalSize = marshalSize + 2;  // samples
+    marshalSize = marshalSize + data.length;
 
-   return marshalSize;
-}
+    return marshalSize;
+  }
 
-public SignalPdu setHeader(RadioCommsHeader pHeader)
-{
+  public SignalPdu setHeader(RadioCommsHeader pHeader)
+  {
     header = pHeader;
     return this;
-}
+  }
 
-public RadioCommsHeader getHeader()
-{
+  public RadioCommsHeader getHeader()
+  {
     return header;
-}
+  }
 
 
-public void setEncodingScheme(int pEncodingScheme)
-{ encodingScheme = pEncodingScheme;
-}
+  public void setEncodingScheme(int pEncodingScheme)
+  { encodingScheme = pEncodingScheme;
+  }
 
-public int getEncodingScheme()
-{ return encodingScheme; 
-}
+  public int getEncodingScheme()
+  { return encodingScheme;
+  }
 
-public void setTdlType(int pTdlType)
-{ tdlType = pTdlType;
-}
+  public void setTdlType(int pTdlType)
+  { tdlType = pTdlType;
+  }
 
-public int getTdlType()
-{ return tdlType; 
-}
+  public int getTdlType()
+  { return tdlType;
+  }
 
-public void setSampleRate(long pSampleRate)
-{ sampleRate = pSampleRate;
-}
+  public void setSampleRate(long pSampleRate)
+  { sampleRate = pSampleRate;
+  }
 
-public long getSampleRate()
-{ return sampleRate; 
-}
+  public long getSampleRate()
+  { return sampleRate;
+  }
 
-public short getDataLength()
-{ return (short)data.length;
-}
+  public short getDataLength()
+  { return (short)data.length;
+  }
 
-/** Note that setting this value will not change the marshalled value. The list whose length this describes is used for that purpose.
- * The getdataLength method will also be based on the actual list length rather than this value. 
- * The method is simply here for java bean completeness.
- */
-public void setDataLength(short pDataLength)
-{ dataLength = pDataLength;
-}
+  /** Note that setting this value will not change the marshalled value. The list whose length this describes is used for that purpose.
+   * The getdataLength method will also be based on the actual list length rather than this value.
+   * The method is simply here for java bean completeness.
+   */
+  public void setDataLength(short pDataLength)
+  { dataLength = pDataLength;
+  }
 
-public void setSamples(short pSamples)
-{ samples = pSamples;
-}
+  public void setSamples(short pSamples)
+  { samples = pSamples;
+  }
 
-public short getSamples()
-{ return samples; 
-}
+  public short getSamples()
+  { return samples;
+  }
 
-public void setData(byte[] pData)
-{ data = pData;
-}
+  public void setData(byte[] pData)
+  { data = pData;
+  }
 
-public byte[] getData()
-{ return data; }
+  public byte[] getData()
+  { return data; }
 
 
-public void marshal(DataOutputStream dos)
-{
+  public void marshal(DataOutputStream dos)
+  {
     super.marshal(dos);
-    try 
+    try
     {
-       dos.writeShort( (short)encodingScheme);
-       dos.writeShort( (short)tdlType);
-       dos.writeInt( (int)sampleRate);
-       dos.writeShort( (short)data.length);
-       dos.writeShort( (short)samples);
-       dos.write(data);
+      header.marshal(dos);
+      dos.writeShort( (short)encodingScheme);
+      dos.writeShort( (short)tdlType);
+      dos.writeInt( (int)sampleRate);
+      dos.writeShort( (short)data.length);
+      dos.writeShort( (short)samples);
+      dos.write(data);
     } // end try 
     catch(Exception e)
-    { 
-      System.out.println(e);}
-    } // end of marshal method
-
-public void unmarshal(DataInputStream dis)
-{
-     super.unmarshal(dis);
-
-    try 
     {
-       encodingScheme = (int)dis.readUnsignedShort();
-       tdlType = (int)dis.readUnsignedShort();
-       sampleRate = dis.readInt();
-       dataLength = dis.readShort();
-       samples = dis.readShort();
-       data = new byte[dataLength];
-       dis.read(data);
+      System.out.println(e);}
+  } // end of marshal method
+
+  public void unmarshal(DataInputStream dis)
+  {
+    super.unmarshal(dis);
+
+    try
+    {
+      header.unmarshal(dis);
+      encodingScheme = dis.readUnsignedShort();
+      tdlType = dis.readUnsignedShort();
+      sampleRate = dis.readInt();
+      dataLength = dis.readShort();
+      samples = dis.readShort();
+      data = new byte[dataLength];
+      dis.read(data);
     } // end try 
-   catch(Exception e)
-    { 
-      System.out.println(e); 
+    catch (Exception e)
+    {
+      System.out.println(e);
     }
- } // end of unmarshal method 
+  } // end of unmarshal method
 
 
-/**
- * Packs a Pdu into the ByteBuffer.
- * @throws java.nio.BufferOverflowException if buff is too small
- * @throws java.nio.ReadOnlyBufferException if buff is read only
- * @see java.nio.ByteBuffer
- * @param buff The ByteBuffer at the position to begin writing
- * @since ??
- */
-public void marshal(java.nio.ByteBuffer buff)
-{
-       super.marshal(buff);
-       buff.putShort( (short)encodingScheme);
-       buff.putShort( (short)tdlType);
-       buff.putInt( (int)sampleRate);
-       buff.putShort( (short)data.length);
-       buff.putShort( (short)samples);
-       buff.put(data);
-    } // end of marshal method
+  /**
+   * Packs a Pdu into the ByteBuffer.
+   * @throws java.nio.BufferOverflowException if buff is too small
+   * @throws java.nio.ReadOnlyBufferException if buff is read only
+   * @see java.nio.ByteBuffer
+   * @param buff The ByteBuffer at the position to begin writing
+   * @since ??
+   */
+  public void marshal(java.nio.ByteBuffer buff)
+  {
+    super.marshal(buff);
+    try {
+      header.marshal(buff);
+      buff.putShort( (short)encodingScheme);
+      buff.putShort( (short)tdlType);
+      buff.putInt( (int)sampleRate);
+      buff.putShort( (short)data.length);
+      buff.putShort( (short)samples);
+      buff.put(data);
+    } catch (Exception e) {
+      System.out.println(e);
+    }
 
-/**
- * Unpacks a Pdu from the underlying data.
- * @throws java.nio.BufferUnderflowException if buff is too small
- * @see java.nio.ByteBuffer
- * @param buff The ByteBuffer at the position to begin reading
- * @since ??
- */
-public void unmarshal(java.nio.ByteBuffer buff)
-{
-       super.unmarshal(buff);
+  } // end of marshal method
 
-       encodingScheme = (int)(buff.getShort() & 0xFFFF);
-       tdlType = (int)(buff.getShort() & 0xFFFF);
-       sampleRate = buff.getInt();
-       dataLength = buff.getShort();
-       samples = buff.getShort();
-       data = new byte[dataLength];
-       buff.get(data);
- } // end of unmarshal method 
+  /**
+   * Unpacks a Pdu from the underlying data.
+   * @throws java.nio.BufferUnderflowException if buff is too small
+   * @see java.nio.ByteBuffer
+   * @param buff The ByteBuffer at the position to begin reading
+   * @since ??
+   */
+  public void unmarshal(java.nio.ByteBuffer buff)
+  {
+    super.unmarshal(buff);
+
+    try {
+      header.unmarshal(buff);
+      encodingScheme = (int) (buff.getShort() & 0xFFFF);
+      tdlType = (int) (buff.getShort() & 0xFFFF);
+      sampleRate = buff.getInt();
+      dataLength = buff.getShort();
+      samples = buff.getShort();
+      data = new byte[dataLength];
+      buff.get(data);
+    }  catch (Exception e) {
+      System.out.println(e);
+    }
+  } // end of unmarshal method
 
 
- /*
-  * The equals method doesn't always work--mostly it works only on classes that consist only of primitives. Be careful.
-  */
-@Override
- public boolean equals(Object obj)
- {
+  /*
+   * The equals method doesn't always work--mostly it works only on classes that consist only of primitives. Be careful.
+   */
+  @Override
+  public boolean equals(Object obj)
+  {
 
     if(this == obj){
       return true;
     }
 
     if(obj == null){
-       return false;
+      return false;
     }
 
     if(getClass() != obj.getClass())
-        return false;
+      return false;
 
     return equalsImpl(obj);
- }
+  }
 
-@Override
- public boolean equalsImpl(Object obj)
- {
-     boolean ivarsEqual = true;
+  @Override
+  public boolean equalsImpl(Object obj)
+  {
+    boolean ivarsEqual = true;
 
     if(!(obj instanceof SignalPdu))
-        return false;
+      return false;
 
-     final SignalPdu rhs = (SignalPdu)obj;
+    final SignalPdu rhs = (SignalPdu)obj;
 
-     if( ! (encodingScheme == rhs.encodingScheme)) ivarsEqual = false;
-     if( ! (tdlType == rhs.tdlType)) ivarsEqual = false;
-     if( ! (sampleRate == rhs.sampleRate)) ivarsEqual = false;
-     if( ! (dataLength == rhs.dataLength)) ivarsEqual = false;
-     if( ! (samples == rhs.samples)) ivarsEqual = false;
-     if( ! (Arrays.equals(data, rhs.data))) ivarsEqual = false;
+    if( ! (encodingScheme == rhs.encodingScheme)) ivarsEqual = false;
+    if( ! (tdlType == rhs.tdlType)) ivarsEqual = false;
+    if( ! (sampleRate == rhs.sampleRate)) ivarsEqual = false;
+    if( ! (dataLength == rhs.dataLength)) ivarsEqual = false;
+    if( ! (samples == rhs.samples)) ivarsEqual = false;
+    if( ! (Arrays.equals(data, rhs.data))) ivarsEqual = false;
 
     return ivarsEqual && super.equalsImpl(rhs);
- }
+  }
 } // end of class
